b12e752dd9bfc54e00ec5a2d60f39c23ece78261
KarenQianShen <shenqian569@163.com>
2016-04-02 22:39:49 +0800

    Added files via upload

---
+++ src/tetromino/ZTetromino.java
@@ -0,0 +1,74 @@
+/**
+ * TTetromino.java
+ *
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ *
+ *    May you do good and not evil.
+ *    May you find forgiveness for yourself and forgive others.
+ *    May you share freely, never taking more than you give.
+ *
+ */
+package tetromino;
+
+import java.awt.Color;
+
+import main.Coordinate;
+import main.GameLabel;
+
+/**
+ * @author Qian Shen
+ * @since 2016-4-2
+ * Z形骨牌
+ */
+public class ZTetromino extends Tetromino {
+	Color color = Color.RED;
+	static final Coordinate[][] block =
+		{
+			{
+				new Coordinate(0, 0),
+				new Coordinate(0, 1),
+				new Coordinate(-1, 1),
+				new Coordinate(1, 0),
+			
+			},
+			{
+				new Coordinate(0, 0),
+				new Coordinate(0, -1),
+				new Coordinate(1, 1),
+				new Coordinate(1, 0),
+			},
+			{
+				new Coordinate(0, 0),
+				new Coordinate(0, 1),
+				new Coordinate(-1, 1),
+				new Coordinate(1, 0),
+			},
+			{
+				new Coordinate(0, 0),
+				new Coordinate(0, -1),
+				new Coordinate(1, 1),
+				new Coordinate(1, 0),
+			},
+		};
+	
+	public ZTetromino(GameLabel _g) {
+		super(_g);
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	
+	Coordinate[] getBlock() {
+		return block[rotationState];
+	}
+	
+	Coordinate[] getRotatedBlock() {
+		return block[(rotationState + 1) % 4];
+	}
+
+	boolean isRotationForbidden() {
+		return false;
+	}
+}
